5f4daca98c926bbcd018267c2fbb821909efe61e
quantmth1908038 <quantmth1908038@fpt.edu.vn>
2020-05-26 13:39:24 +0700

    Lab_07

---
+++ Lab_07/Employee/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Employee
+{
+    class Employee
+    {
+        public static int EmpId = 20;
+        public static string EmpName = "James";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee ID: " + EmpId);
+            Console.WriteLine("Employee Name: " + EmpName);
+        }
+
+        public string Name = "Wilson";
+        private float _salary;
+        protected float Salary;
+
+        internal static int NumOne = 3;
+
+
+    }
+}
---
+++ Lab_07/TestProgram/TestProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestProgram
+{
+    class TestProgram
+    {
+        void Count(int boys, int girls)
+        {
+            Console.WriteLine(boys + girls);
+        }
+
+        static void Main(string[] args)
+        {
+            TestProgram test = new TestProgram();
+            test.Count(boys: 16, girls: 24);
+        }
+    }
+}
